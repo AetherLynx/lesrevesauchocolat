17c63cda35c832116282eb38c6edcdf0519f277c
Lynx <81195994+AetherLynx@users.noreply.github.com>
2024-03-28 20:01:29 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //CONEXIÓN A LA BD
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "lrac_bd";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-?>
