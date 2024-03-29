72a0647fcd34dec395085ad298a20207788c46cc
Lynx <81195994+AetherLynx@users.noreply.github.com>
2024-03-28 19:59:43 -0500

    Delete LRAC_V9 directory

--- LRAC_V9/conns/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../phpfuncs/main.php");
-if (!isset($_SESSION)) {
-    session_start();
-}
-
-session_unset(); //clear ALL session variables
-setPopup("Tu sesión fue cerrada exitosamente.", "../login.php");
